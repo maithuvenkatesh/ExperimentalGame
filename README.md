358a8a2cd6accdb6023071d36fee00becdd09361
Maithu Venkatesh <maithuvenkatesh@gmail.com>
2013-12-30 23:24:41 +0000

    Basic static page

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 This is a game Thea and I created while we were extremely bored. 
 The level of boredom cannot be conveyed using words alone. You just had to be there.
+
+IDEA
+Minion Chatbot
+
+FEATURES
+
+
--- src/game.js
+++ src/game.js
@@ -1,8 +1,14 @@
-Game = {
-  // Initialize and start our game
-  start: function() {
-    // Start crafty and set a background color so that we can see it's working
-    Crafty.init(480, 320);
-    Crafty.background('green');
-  }
+// Minion image
+var minionReady = false;
+var minionImage = new Image();
+minionImage.onload = function() {
+	minionReady = true;
+};
+minionImage.src = "assets/minion.png";
+
+// Render objects
+var render = function() {
+	if(minionReady) {
+		context.drawImage(minionImage, minion.x, minion.y);
+	}
 }
